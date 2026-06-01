ee716a575169098194a8d3ba9308519af13a76c8
Prince <prince206122king@gmail.com>
2026-06-01 21:51:32 +0530

    Implement Two Sum solution in C++

---
+++ Week_1/Day_1/Two_Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j,n=nums.size();
+        for(i=0;i<n-1;i++)
+        {
+           for(j=i+1;j<n;j++)
+           {
+               if(nums[i]+nums[j]==target)
+               return{i,j};
+           }
+        }
+        return{};
+    }
+};
