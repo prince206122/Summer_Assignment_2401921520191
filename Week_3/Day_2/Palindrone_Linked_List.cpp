3aecb151034640545d855208bc82537f3587d79e
Prince <prince206122king@gmail.com>
2026-06-16 22:01:08 +0530

    Implement palindrome check for linked list

---
+++ Week_3/Day_2/Palindrone_Linked_List.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head->next==NULL)
+        return 1;
+        ListNode *temp=head;
+        int count=0;
+        while(temp)
+        {
+            count++;
+            temp=temp->next;
+        }
+        count/=2;
+        ListNode *prev=NULL,*curr=head;
+        while(count--)
+        {
+            prev=curr;
+            curr=curr->next;
+        }
+        prev->next=NULL;
+
+        ListNode* fut=NULL;
+        prev=NULL;
+        while(curr)
+        {
+            fut=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=fut;
+
+        }
+        ListNode *head1=head,*head2=prev;
+        while(head1)
+        {
+            if(head1->val!=head2->val)
+            return 0;
+            head1=head1->next;
+            head2=head2->next;
+        }
+        return 1;
+    }
+};
