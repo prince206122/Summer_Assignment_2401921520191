46d3ac8d891ed8c9e3ac69570697166d1f148dbf
Prince <prince206122king@gmail.com>
2026-06-24 23:38:37 +0530

    Add Validate_BST.cpp for BST validation
    Implement inorder traversal to validate a binary search tree.

---
+++ Week_4/Day_3/Validate_BST.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void inorder(TreeNode *root,vector<int>&ans)
+    {
+        if(!root)return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    bool isValidBST(TreeNode* root) {
+        if(!root->left && !root->right)return 1;
+        vector<int>ans;
+        inorder(root,ans);
+        for(int i=1;i<ans.size();i++)
+        {
+            if(ans[i]<=ans[i-1])return 0;
+        }
+        return 1;
+    }
+};
